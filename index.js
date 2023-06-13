43fd280ae63541a6644c00179a4ff13c4ef8d2da
Yong Chee Ho <yongcheeho@gmail.com>
2023-06-13 11:28:58 +0800

    Added POST endpoint

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import prisma from "./src/utils/prisma.js"
 const app = express()
 const port = process.env.PORT || 8080
 
+app.use(express.json())
+
 app.get('/', async (req, res) => {
     const allUsers = await prisma.user.findMany()
     res.json(allUsers)
@@ -12,3 +14,27 @@ app.get('/', async (req, res) => {
 app.listen(port, () => {
   console.log(`App started; listening on port ${port}`)
 })
+
+app.post(`/users`, async (req, res) => {
+  try {
+    const data = req.body
+    const users = await prisma.user.create({
+    data: {
+    },
+  })
+  res.json(users)
+
+  } catch (err) {
+    if (err instanceof prisma.PrismaClientKnownRequestError && err.code === 'P2002') {
+      const formattedError = {}
+      formattedError[`${err.meta.target[0]}`] = 'already taken'
+
+      return res.status(500).send({
+        error: formattedError
+      });
+    }
+
+  } throw err
+
+})
+

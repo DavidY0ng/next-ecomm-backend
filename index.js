747e144cc5393fb42479d5f0ab3ccad07e303cde
Yong Chee Ho <yongcheeho@gmail.com>
2023-06-14 10:51:59 +0800

    Added user can sign in feature

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import express from "express"
 import prisma from "./src/utils/prisma.js"
 import { Prisma } from "@prisma/client"
+import bcrypt from "bcryptjs"
+import cors from "cors"
+import { signAccessToken } from "./src/utils/jwt.js"
+
 
 const app = express()
 const port = process.env.PORT || 8080
@@ -35,7 +39,26 @@ function validateUser(input) {
   return validationErrors
 }
 
+function validateLogin(input) {
+  const validationErrors = {}
+
+  if (!('email' in input) || input['email'].length == 0) {
+    validationErrors['email'] = 'cannot be blank'
+  }
+
+  if (!('password' in input) || input['password'].length == 0) {
+    validationErrors['password'] = 'cannot be blank'
+  }
+
+  if ('email' in input && !input['email'].match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)) {
+    validationErrors['email'] = 'is invalid'
+  }
+
+  return validationErrors
+}
+
 app.use(express.json())
+app.use(cors())
 
 app.get('/', async (req, res) => {
     const allUsers = await prisma.user.findMany()
@@ -48,18 +71,16 @@ app.listen(port, () => {
 
 app.post(`/users`, async (req, res) => {
   try {
+    const data = req.body
+
     const validationErrors = validateUser(data)
     if (Object.keys(validationErrors).length != 0) return res.status(400).send({
       error: validationErrors
     })
 
-    const { name, email, password } = req.body
+    data.password = bcrypt.hashSync(data.password, 8);
     const user = await prisma.user.create({
-    data: {
-      name,
-      email,
-      password,
-    },
+    data
   })
   res.json(filter(user, 'id', 'name', 'email'))
 
@@ -76,5 +97,32 @@ app.post(`/users`, async (req, res) => {
   } 
 })
 
+app.post('/sign-in', async (req, res) => {
+  const data = req.body
+
+  const validationErrors = validateLogin(data)
+
+  if (Object.keys(validationErrors).length != 0) return res.status(400).send({
+    error: validationErrors
+  })
+
+  const user = await prisma.user.findUnique({
+    where: {
+      email: data.email
+    }
+  })
+
+  if (!user) return res.status(401).send({
+    error: 'Email address or password not valid'
+  })
+
+  const checkPassword = bcrypt.compareSync(data.password, user.password)
+  if (!checkPassword) return res.status(401).send({
+    error: 'Email address or password not valid'
+  })
+
+  const accessToken = await signAccessToken(user)
+  return res.json({ accessToken })
+})
 
 

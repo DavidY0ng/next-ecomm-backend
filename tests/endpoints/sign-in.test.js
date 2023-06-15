51cc60cc231b1be3ad97cf66651f0af190b5307a
Yong Chee Ho <yongcheeho@gmail.com>
2023-06-15 14:35:59 +0800

    Sign in test is working

--- tests/endpoints/sign-in.test.js
+++ tests/endpoints/sign-in.test.js
@@ -31,7 +31,6 @@ describe ("POST /sign-in", () => {
     })
 
     it("with valid data should return token", async () => {
-      // Create the user
       const createUserResponse = await request(app)
         .post("/users")
         .send(user)
@@ -39,37 +38,33 @@ describe ("POST /sign-in", () => {
   
       expect(createUserResponse.statusCode).toBe(200);
   
-      // Sign in with the user
       const signInResponse = await request(app)
         .post("/sign-in")
         .send(user)
         .set('Accept', 'application/json');
   
       expect(signInResponse.statusCode).toBe(200);
-      expect(signInResponse.body.token).toBeTruthy;
-      console.log({ "signInResponse.body.token": signInResponse.body.token})
+      expect(signInResponse.body.accessToken).toBeTruthy();
     });
 
-    // it("with invalid email should fail", async () => {
-    //   // Create the user
-    //   const createUserResponse = await request(app)
-    //     .post("/users")
-    //     .send(user)
-    //     .set('Accept', 'application/json');
+    it("with invalid email should fail", async () => {
+      const createUserResponse = await request(app)
+        .post("/users")
+        .send(user)
+        .set('Accept', 'application/json');
   
-    //   expect(createUserResponse.statusCode).toBe(200);
+      expect(createUserResponse.statusCode).toBe(200);
   
-    //   // Sign in with the user
-    //   const signInResponse = await request(app)
-    //   user.email = "dave@example.com"
-    //     .post("/sign-in")
-    //     .send(user)
-    //     .set('Accept', 'application/json');
+      const signInResponse = await request(app)
+      user.email = "dave@example.com"
+        .post("/sign-in")
+        .send(user)
+        .set('Accept', 'application/json');
   
-    //   expect(signInResponse.statusCode).toBe(402);
-    //   expect(signInResponse.body.error).toBeTruthy
-    //   expect(signInResponse.body.error.email).toBe('Email address or password not valid')
-    // });
+      expect(signInResponse.statusCode).toBe(402);
+      expect(signInResponse.body.error).toBeTruthy
+      expect(signInResponse.body.error.email).toBe('Email address or password not valid')
+    });
   
 
 })

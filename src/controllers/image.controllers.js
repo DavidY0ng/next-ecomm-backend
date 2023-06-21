b16615876a908cf18a583f5fe8104b3151b4cdc9
Yong Chee Ho <yongcheeho@gmail.com>
2023-06-21 14:39:29 +0800

    Still trying to fix delete function

--- src/controllers/image.controllers.js
+++ src/controllers/image.controllers.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import { Prisma } from "@prisma/client"
 import prisma from "../utils/prisma.js"
+import auth from '../middlewares/auth.js'
 const router = express.Router()
 
 
@@ -25,5 +26,27 @@ router.post('/', async (req, res) => {
     throw err
   })
 })
+
+router.delete('/delete', auth , async (req, res) => {
+  console.log(req.user.payload.id)
+  const image = await prisma.image.findUnique({
+    where: {
+      id: req.user.payload.id
+    }
+  })
+  
+  // we have access to `req.user` from our auth middleware function (see code above where the assignment was made)
+  if (req.user.id != image.sellerId) {
+      return res.status(401).send({"error": "Unauthorized"})
+  }
+  
+  const deleteImage = await prisma.image.delete({
+    where: {
+     id
+    },
+  })
+  // some code
+})
+
 
 export default router

a9bd7b9fef7c4df4358d4e81d056adcedda8e6c9
Yong Chee Ho <yongcheeho@gmail.com>
2023-06-23 16:20:07 +0800

    Stripe payment checkout working except price data

--- src/controllers/checkout.controllers.js
+++ src/controllers/checkout.controllers.js
@@ -1,29 +1,42 @@
 // This is your test secret API key.
 import express from 'express';
 import Stripe from 'stripe'
+import prisma from "../utils/prisma.js"
 const stripe = new Stripe ('sk_test_51NLH7lDSZDxIvwntMu2KfIg6n8VKlCXJBGrh5OWusAuADBw6EZxz3jGP8Q4625BklCKm9s6iD8m1nGrV6DL1y7TF00ppTTwxyb')
 const router = express.Router()
 
 router.post('/', async (req, res) => {
+  
+  const imageId = req.body
+  const imageData = await prisma.image.findUnique ({
+    where : {
+      id : parseInt(imageId.id)
+    }
+  })
+  console.log(imageData.path)
   const session = await stripe.checkout.sessions.create({
     line_items: [
       {
         price_data: {
           currency: 'usd',
           product_data: {
-            name: 'T-shirt',
+            name: imageData.title,
+            description: imageData.description,
+            images: [imageData.path
+            ]
           },
-          unit_amount: 1900,
+          unit_amount: 2000,
         },
         quantity: 1,
       },
     ],
     mode: 'payment',
-    success_url: 'http://localhost:5713/success',
-    cancel_url: 'http://localhost:5713/cancel',
+    success_url: 'http://localhost:5173/checkout/success/',
+    cancel_url: 'http://localhost:5173/checkout/cancel/',
   });
+  return res.json(session.url)
 
-  res.redirect(303, session.url);
+  // res.redirect(303, session.url);
 });
 
 export default router
